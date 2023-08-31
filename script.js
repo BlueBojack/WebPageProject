b6bcbcbb08a2e042d44e6c54b5d6b822c827451a
Sven <chanck.mds@gmail.com>
2023-08-31 08:27:48 +0200

    fixed sizing problems and added content to featured-block

--- script.js
+++ script.js
@@ -20,16 +20,22 @@ scrollButtons[0].addEventListener('click', () => {
 
 function toggleScrollButtonsVisibility() {
 if (i > 0 & i<=1) {
-    scrollButtons[0].style.left = ('48px');
+    scrollButtons[0].style.left = ('calc(4.5rem - 41px)');
+    scrollButtons[0].style.opacity = ('100%')
     scrollButtons[1].style.right = ('var(--handle-size)');
+    scrollButtons[1].style.opacity = ('100%')
 } 
 else if (i > 1){
-    scrollButtons[0].style.left = ('48px');
-    scrollButtons[1].style.right = ('-300px');
+    scrollButtons[0].style.left = ('calc(4.5rem - 41px)');
+    scrollButtons[0].style.opacity = ('100%')
+   
+    scrollButtons[1].style.opacity = ('0')
 }
 else  {
-    scrollButtons[0].style.left = ('-300px');
+    
+    scrollButtons[0].style.opacity = ('0')
     scrollButtons[1].style.right = ('var(--handle-size)');
+    scrollButtons[1].style.opacity = ('100%')
 }
 
 }
