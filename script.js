55586b2ddc52ee8ba7a90ef274634f34528bba34
BlueBojack <curtis.ducasse1g@gmail.com>
2023-09-07 15:13:59 +0200

    Finalized the project

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let i = 0;
 const scrollButtons = document.querySelectorAll('.handle');
 const cardsWrapper = document.querySelectorAll('.slider');
@@ -15,54 +14,27 @@ scrollButtons[0].addEventListener('click', () => {
     i-=1;
     toggleScrollButtonsVisibility();
     cardsWrapper[0].scrollLeft -= 1000;
-    
+
 });
 
 function toggleScrollButtonsVisibility() {
 if (i > 0 & i<=1) {
-    scrollButtons[0].style.left = ('48px');
+    scrollButtons[0].style.left = ('calc(4.5rem - 41px)');
+    scrollButtons[0].style.opacity = ('100%')
     scrollButtons[1].style.right = ('var(--handle-size)');
+    scrollButtons[1].style.opacity = ('100%')
 } 
 else if (i > 1){
-    scrollButtons[0].style.left = ('48px');
-    scrollButtons[1].style.right = ('-300px');
+    scrollButtons[0].style.left = ('calc(4.5rem - 41px)');
+    scrollButtons[0].style.opacity = ('100%')
+
+    scrollButtons[1].style.opacity = ('0')
 }
 else  {
-    scrollButtons[0].style.left = ('-300px');
+
+    scrollButtons[0].style.opacity = ('0')
     scrollButtons[1].style.right = ('var(--handle-size)');
-}
-
-}
-
-//second
-let j = 0;
-scrollButtons[3].addEventListener('click', () => {
-    j+=1;
-    cardsWrapper[1].scrollLeft += 1000;
-    toggleScrollButtonsVisibility2();
-    console.log(j)
-
-});
-
-scrollButtons[2].addEventListener('click', () => {
-    j-=1;
-    toggleScrollButtonsVisibility2();
-    cardsWrapper[1].scrollLeft -= 1000;
-    
-});
-
-function toggleScrollButtonsVisibility2() {
-if (j > 0 & j<=1) {
-    scrollButtons[2].style.left = ('48px');
-    scrollButtons[3].style.right = ('var(--handle-size)');
-} 
-else if (j > 1){
-    scrollButtons[2].style.left = ('48px');
-    scrollButtons[3].style.right = ('-300px');
-}
-else  {
-    scrollButtons[2].style.left = ('-300px');
-    scrollButtons[3].style.right = ('var(--handle-size)');
+    scrollButtons[1].style.opacity = ('100%')
 }
 
 }
